81b797fb2cc2f96ee23faa13a22d7d41aa18f7e0
Nethu2311 <nethrapugal1995@gmail.com>
2022-11-20 13:00:00 +0530

    send a push

---
+++ src/org/test/Sample.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Sample {
+
+	private void a() {
+		System.out.println("nethra");
+	}
+	
+	public static void main(String[] args) {
+		Sample a = new Sample();
+		a.a();
+	}
+}
